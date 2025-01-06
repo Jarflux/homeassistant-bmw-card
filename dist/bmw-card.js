e13f747274fd108f369104c747e07d2de6e0b859
Ben Oeyen <ben.oeyen@gmail.com>
2025-01-06 20:16:44 +0100

    trying to get the card to show up in the ui again.

--- dist/bmw-card.js
+++ dist/bmw-card.js
@@ -66,8 +66,8 @@ window.customCards = window.customCards || [];
 window.customCards.push({
     type: "bmw-card",
     name: "BMW Card",
-    description: "BMW Card to show car data from Connected Drive integration.",
-    documentationURL: "https://github.com/Jarflux/homeassistant-bmw-card/blob/master/README.md"
+    preview: false,
+    description: "BMW Card to show car data from Connected Drive integration."
 });
 
 
